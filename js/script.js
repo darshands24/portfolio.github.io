fab63a471089606a0e2d10f44953f35a9ad9b687
Darshan D S <156211877+darshands24@users.noreply.github.com>
2024-12-30 18:18:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // =========typing animation========//
 var typed=new Typed(".typing",{
-    strings:["","Software Engineer","","Web Developer","","Software Developer","","Web Designer","","Coder",""],
+    strings:["","Software Engineer","","Web Developer","","Software Developer","","Web Designer",""],
     typeSpeed:200,
     BackSpeed:60,
     loop:true
